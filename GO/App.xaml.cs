6b06ffd7d8eda54def05c0cc3dc670129c5b9c05
Chikondi Primrose Nkhoma <chikondinkhoma51@gmail.com>
2022-03-30 16:19:42 +0200

    initial commit

---
+++ GO/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using GO.Services;
+using GO.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GO
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ GO/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GO.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GO.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
