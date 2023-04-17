a880e3a0dbc0899926893e25b417979074f1c7cf
RooseViper <65874617+RooseViper@users.noreply.github.com>
2023-04-17 09:51:05 +0200

    commt

--- GO/Views/GoalTask/WeeklyTask.xaml.cs
+++ GO/Views/GoalTask/WeeklyTask.xaml.cs
@@ -19,16 +19,14 @@ namespace GO.Views.GoalTask
     public partial class WeeklyTask : ContentPage
     {
         public string weekId { get; set; }
+        private int totalSubtaks = 0;
+   
+        private static int completedtasks = 0;
+        private static int UncompletedTasks = 0;
+        private static int completedSubtasks = 0;
+        private static int UncompletedSubtasks = 0;        
        
-        // properties for Dows Id
-        private Week GetWeek;
-        private int MonId;
-        private int TueId;
-        private int WedId;
-        private int ThurId;
-        private int FriId;
-        private int SatId;
-        private bool Isvalid = false;
+
         public IDataGoal<Models.Goal> datagoal { get; }
         public IDataTask<Models.GoalTask> DataTask { get; }
         public IDataWeek<Week> dataWeek { get; }
@@ -46,8 +44,7 @@ namespace GO.Views.GoalTask
             dataDow = DependencyService.Get<IDataDow<Models.DOW>>();
             dataSubtask = DependencyService.Get<IDataSubtask<Models.Subtask>>();
             BindingContext = new WeeklyTaskViewModel();
-            TimeSpan ts = new TimeSpan(0, 0, 0, 5);
-            chartView.Chart = new BarChart { Entries = entries, IsAnimated=true,AnimationDuration=ts,  AnimationProgress= 5,LabelTextSize = 30 };
+           
 
         }
         protected async override void OnAppearing()
@@ -56,9 +53,50 @@ namespace GO.Views.GoalTask
             int.TryParse(weekId, out var result);
             // get the week throgh the id
             var Week = await dataWeek.GetWeekAsync(result);
+            var Weektasks = await DataTask.GetTasksAsync(Week.GoalId, Week.Id);
+            // divide tasks count with 514
+            var dividedTasks = 0;
+            var dividedsubTasks = 0;
+            // loop trhu the tasks to get subtasks
+            foreach (var task in Weektasks)
+            {
+                //get completed tasks
+                if (task.IsCompleted)
+                    completedtasks += 1;
+                else if (!task.IsCompleted)
+                    UncompletedTasks += 1;
+                //get subtasks
+                var subtasks = await dataSubtask.GetSubTasksAsync(task.Id);
+                if (subtasks.Count() > 0)
+                    dividedsubTasks = 514 / subtasks.Count();
+                foreach(var subtask in subtasks)
+                {
+                    //get completed subtasks
+                    if (subtask.IsCompleted)
+                        completedtasks += 1;
+                    else if (!task.IsCompleted)
+                        UncompletedSubtasks += 1;
+                }                
+                totalSubtaks += subtasks.Count();            
+                
+            }
+            if (Weektasks.Count() > 0)
+            {
+                dividedTasks = 514 / Weektasks.Count();
+                completedtasks = completedtasks * dividedTasks;
+                UncompletedTasks = UncompletedTasks * dividedTasks;
+            }
+
+            if (completedSubtasks >0 || UncompletedSubtasks>0)
+            {               
+                completedSubtasks = completedtasks * dividedTasks;
+                UncompletedSubtasks = UncompletedTasks * dividedTasks;
+            }
+
+
             if (BindingContext is WeeklyTaskViewModel wvm)
             {
-                await wvm.CalculateTotalWeekPercentage(Week);
+                await wvm.CalculateTotalWeekPercentage(Week);                           
             }
             progressring.Progress = Week.Progress;
             wkpercentage.Text = Week.AccumulatedPercentage.ToString();
@@ -66,33 +104,46 @@ namespace GO.Views.GoalTask
             target.Text = Week.TargetPercentage.ToString();
             startdate.Text = Week.StartDate.ToShortDateString();
             enddate.Text = Week.EndDate.ToShortDateString();
-        }
-        private List<ChartEntry> entries = new List<ChartEntry>
-        {
-           new ChartEntry(514)
+            alltasks.Text = Weektasks.Count().ToString();
+            allsubtasks.Text = totalSubtaks.ToString();
+            
+            List<ChartEntry> entries = new List<ChartEntry>
+            {
+           new ChartEntry(completedtasks)
             {
                 Label = "CT",
                 ValueLabel = "5",
                 Color = SKColor.Parse("#012a4a")
             },
-            new ChartEntry(248)
+            new ChartEntry(UncompletedTasks)
             {
                 Label = "UT",
                 ValueLabel = "648",
                 Color = SKColor.Parse("#ff8600")
             },
-            new ChartEntry(248)
+            new ChartEntry(completedSubtasks)
             {
                 Label = "CS",
                 ValueLabel = "648",
                 Color = SKColor.Parse("#89c2d9")
             },
-            new ChartEntry(248)
+            new ChartEntry(UncompletedSubtasks)
             {
                 Label = "US",
                 ValueLabel = "648",
                 Color = SKColor.Parse("#ffb5a7")
-            },      
+            },
         };
+
+
+            TimeSpan ts = new TimeSpan(0, 0, 0, 5);
+            chartView.Chart = new BarChart { Entries = entries, IsAnimated = true, AnimationDuration = ts, AnimationProgress = 5, LabelTextSize = 30 };
+            completedSubtasks = 0;
+            completedtasks = 0;
+            UncompletedSubtasks = 0;
+            UncompletedTasks = 0;
+        }
+       
+        
     }
 }
